98251abb3456e58d3b5fa06f5d52fe86d6e21dea
mutahi-simon <mutahi.simon21@students.dkut.ac.ke>
2024-11-11 09:16:36 +0300

    initial commit

---
+++ jinjaApp/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+def base(request):
+    return render(request,'base.html')
+
+def about(request):
+    return render(request,'about.html')
+
+def contact(request):
+    return render(request,'contact.html')
+
+def home(request):
+    return render(request,'home.html')
